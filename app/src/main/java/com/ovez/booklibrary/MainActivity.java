c0c9daed196793f22f0db8e3a7081f2b0ed8ab9b
Ovezmyrat ARNAZAROV <ala200bama@gmail.com>
2022-02-21 00:59:47 -0600

    added layout views for main activity

--- app/src/main/java/com/ovez/booklibrary/MainActivity.java
+++ app/src/main/java/com/ovez/booklibrary/MainActivity.java
@@ -3,12 +3,26 @@ package com.ovez.booklibrary;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btn_allBooks, btn_alreadyRead, btn_wantToRead, btn_currentlyReading, btn_favourite, btn_about;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initViews();
+    }
+
+    private void initViews() {
+        btn_allBooks = findViewById(R.id.btn_allBooks);
+        btn_alreadyRead = findViewById(R.id.btn_alreadyRead);
+        btn_wantToRead = findViewById(R.id.btn_wantToRead);
+        btn_currentlyReading = findViewById(R.id.btn_currentlyReading);
+        btn_favourite = findViewById(R.id.btn_favourite);
+        btn_about = findViewById(R.id.btn_about);
     }
 }
