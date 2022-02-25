573b58367cde8780bd36f243555db6f2627a802e
Ovezmyrat ARNAZAROV <ala200bama@gmail.com>
2022-02-25 00:18:32 -0600

    created FavouriteBook activity

---
+++ app/src/main/java/com/ovez/booklibrary/FavouriteBookActivity.java
@@ -0,0 +1,33 @@
+package com.ovez.booklibrary;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class FavouriteBookActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private BookRecViewAdapter adapter;
+
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_favourite_book);
+
+        recyclerView = findViewById(R.id.favouriteBooksRecView);
+        adapter = new BookRecViewAdapter(this);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        adapter.setBooks(Utils.getFavouriteBooks());
+    }
+}
